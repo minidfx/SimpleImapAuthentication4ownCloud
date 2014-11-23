f1e53821700a7b3cf87dc4abb2a70c5f2d5f4643
Burgy Benjamin <minidfx@gmail.com>
2014-11-23 22:00:53 +0100

    IS-1: Fix template of the admin console.

--- templates/settings.php
+++ templates/settings.php
@@ -4,21 +4,19 @@
 
 		<p>
 			<label for="imap_uri"><?php p($l->t('Server Host')); ?></label>
-			<input type="text" id="imap_uri" name="imap_uri"
-			       value="<?php p($_['imap_uri']); ?>">
+			<input type="text" name="imap_uri" class="imap_uri" value="<?php p($_['imap_uri']); ?>">
 		</p>
 
 		<p>
 			<label for="imap_port"><?php p($l->t('Server Port')); ?></label>
-			<input type="password" id="imap_port" name="imap_port"
-			       value="<?php p($_['imap_port']); ?>">
+			<input type="text" name="imap_port" value="<?php p($_['imap_port']); ?>">
+			<span id="imap_settings_msg" class="msg success" style="display: none;">Saved</span>
 		</p>
-
+		
 		<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>" id="requesttoken">
 
 		<p>
 			<?php p($l->t('ownCloud will send the user credentials to this Host. This plugin check it can open the INBOX of the user.')); ?>
-			<span id="imap_settings_msg" class="msg success" style="display: none;">Saved</span>
 		</p>
 	</form>
 </div>
