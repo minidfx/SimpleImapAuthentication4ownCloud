cc46178abc76e189f84296b0ba56f464e16f7b1d
Burgy Benjamin <minidfx@gmail.com>
2014-11-28 21:55:38 +0100

    IS-1: Remove unused configuations.

--- appinfo/app.php
+++ appinfo/app.php
@@ -11,7 +11,6 @@ namespace OCA\user_imapauth\AppInfo;
 
 use OCA\user_imapauth\App\IMAPAuthenticatorApp;
 use OCP\App;
-use OCP\Util;
 
 if (!defined('APP_ID'))
 {
@@ -25,22 +24,11 @@ $application = new IMAPAuthenticatorApp();
 App::addNavigationEntry(array(
 
 	                        // the string under which your app will be referenced in owncloud
-	                        'id'    => APP_ID,
-
-	                        // sorting weight for the navigation. The higher the number, the higher
-	                        // will it be listed in the navigation
-	                        'order' => 10,
-
-	                        // the route that will be shown on startup
-	                        'href'  => Util::linkToRoute(APP_ID . '.page.index'),
-
-	                        // the icon that will be shown in the navigation
-	                        // this file needs to exist in img/example.png
-	                        'icon'  => Util::imagePath(APP_ID, 'nav-icon.png'),
+	                        'id'   => APP_ID,
 
 	                        // the title of your application. This will be used in the
 	                        // navigation or on the settings page of your app
-	                        'name'  => $application->getL10N()->t('IMAP User Authentication')
+	                        'name' => $application->getL10N()->t('IMAP User Authentication')
                         ));
 
 /**
