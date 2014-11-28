18c5a0874ee7433f5ca44d253278fac0c3320461
Burgy Benjamin <minidfx@gmail.com>
2014-11-28 21:53:09 +0100

    IS-1: Remove unused files.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: minidfx
- * Date: 23.11.14
- * Time: 00:10
- */
-
--- templates/main.php
+++
@@ -1,2 +0,0 @@
-<p>Application for authenticating users through IMAP. You have to configure the authenticator from the admin
-	<a href="index.php/settings/admin">console</a>.</p>
