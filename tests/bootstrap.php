5de9b5fc92d187589aff95faac46f61cb7911e40
Burgy Benjamin <minidfx@gmail.com>
2014-11-30 17:37:11 +0100

    IS-2: Add the ownCloud constants for backends.
    Bad way but don't have another solutions at the moment.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,6 +10,15 @@
 /** @noinspection SpellCheckingInspection */
 define('APP_ID', 'user_imapauth');
 
+define('OC_USER_BACKEND_CREATE_USER', 0x00000001);
+define('OC_USER_BACKEND_SET_PASSWORD', 0x00000010);
+define('OC_USER_BACKEND_CHECK_PASSWORD', 0x00000100);
+define('OC_USER_BACKEND_GET_HOME', 0x00001000);
+define('OC_USER_BACKEND_GET_DISPLAYNAME', 0x00010000);
+define('OC_USER_BACKEND_SET_DISPLAYNAME', 0x00100000);
+define('OC_USER_BACKEND_PROVIDE_AVATAR', 0x01000000);
+define('OC_USER_BACKEND_COUNT_USERS', 0x10000000);
+
 spl_autoload_register(function ($class)
 {
 	/** @var string $startsWith */
